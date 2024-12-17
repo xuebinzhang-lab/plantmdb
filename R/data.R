e39a6dcf5c625db193181e5bedfb6f2eff4c91a8
ShawnWx2009 <shawnwang2016@126.com>
2024-12-17 10:11:04 +0800

    1st init

---
+++ R/data.R
@@ -0,0 +1,86 @@
+#' @title MoNA - MassBank of North America database 0.0.4
+#' @description MassBank of North America database with classification
+#' @author Shawn Wang
+#' \email{shawnwang2016@@126.com}
+#' @format A databaseClass object.
+"mona_database0.0.4"
+
+##------------------------------------------------------------------------------
+#' @title massbank database database 0.0.4
+#' @description massbank database database 0.0.4
+#' @author Shawn Wang
+#' \email{shawnwang2016@@126.com}
+#' @format A databaseClass object.
+"massbank_database0.0.4"
+
+##------------------------------------------------------------------------------
+#' @title PlaSMA_database0.0.1
+#' @description PlaSMA_database0.0.1
+#' @author Shawn Wang
+#' \email{shawnwang2016@@126.com}
+#' @format A databaseClass object.
+"plasma_database0.0.1"
+
+##------------------------------------------------------------------------------
+#' @title respect_database0.0.1
+#' @description respect_database0.0.1
+#' @author Shawn Wang
+#' \email{shawnwang2016@@126.com}
+#' @format A databaseClass object.
+"respect_database0.0.1"
+
+##------------------------------------------------------------------------------
+#' @title Plant kegg database 0.0.1
+#' @description kegg_plant_database0.0.1
+#' @author Shawn Wang
+#' \email{shawnwang2016@@126.com}
+#' @format A databaseClass object.
+"kegg_plant_database0.0.1"
+
+##------------------------------------------------------------------------------
+#' @title zea mays plantcyc database 0.0.1
+#' @description zea mays plantcyc database 0.0.1
+#' @author Shawn Wang
+#' \email{shawnwang2016@@126.com}
+#' @format A databaseClass object.
+"zma_plantcyc_database0.0.1"
+
+##------------------------------------------------------------------------------
+#' @title ath plantcyc database 0.0.1
+#' @description ath plantcyc database 0.0.1
+#' @author Shawn Wang
+#' \email{shawnwang2016@@126.com}
+#' @format A databaseClass object.
+"ath_plantcyc.database0.0.1"
+
+##------------------------------------------------------------------------------
+#' @title knapsack agri database 0.0.1
+#' @description knapsack agri database 0.0.1
+#' @author Shawn Wang
+#' \email{shawnwang2016@@126.com}
+#' @format A databaseClass object.
+"knapsack_agri_database0.0.1"
+
+##------------------------------------------------------------------------------
+#' @title metaboBASE database 1.0.0
+#' @description metaboBASE database 1.0.0
+#' @author Shawn Wang
+#' \email{shawnwang2016@@126.com}
+#' @format A databaseClass object.
+"metabobase_database0.0.1"
+
+##------------------------------------------------------------------------------
+#' @title orbitrap_database0.0.3
+#' @description orbitrap_database0.0.3
+#' @author Xiaotao Shen
+#' \email{shenxt1990@@163.com}
+#' @format A databaseClass object.
+"orbitrap_database0.0.3"
+
+##------------------------------------------------------------------------------
+#' @title pm Database classyFire 0.0.1
+#' @description in-build pmDatabse classyFire
+#' @author Shawn Wang
+#' \email{shawnwang2016@@126.com}
+#' @format A databaseClass object.
+"class.database"
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # plantmdb
 specialized metabolomics database of plant. for tidymass and metid
+
+# citation
+Xiao Wang, Shuang Liang, Wenqi Yang, Ke Yu, Fei Liang, Bing Zhao, Xiang Zhu, Chao Zhou, Luis A. J. Mur, Jeremy A. Roberts, Junli Zhang and Xuebin Zhang. 2024 “MetMiner: A user-friendly pipeline for large-scale plant metabolomics data analysis. ” Journal of Integrative Plant Biology  https://doi.org/10.1111/jipb.13774 
+
+# Highlight
+
+1. Add classification information；  
+
+2. Remove non-plant source metabolites for MS1 database;  
+
+3. For tidymass and metMiner users.  
+
+# database
+
+MS2 spectra: 
+
+- MassBank of North America database with classification (MoNA)
+
+- Massbank database database  (Massbank)
+
+- Plant Specialized Metabolome Annotation (PlaSMA)
+
+- A collection of literature and in-house MSn spectra data for the plant metabolomics research (ReSpect)  
+
+- orbitrap_database
+
+MS1 spectra:
+
+- A comprehensive species-metabolite relationships database (KNApSAcK, https://www.knapsackfamily.com/KNApSAcK/).
+
+- KEGG
+
+- PlantCyc
+
+Classification database
